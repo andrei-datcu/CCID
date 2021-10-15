364b0c13f70c4c85e75b6578467f3d8495d3ce01
Andrei <datcuandrei@gmail.com>
2021-10-15 16:51:13 +0300

    Fix a timeout on a lenovo tablet

--- src/ifdhandler.c
+++ src/ifdhandler.c
@@ -197,7 +197,7 @@ static RESPONSECODE CreateChannelByNameOrChannel(DWORD Lun,
 		/* Do not use a fixed 100 ms value but compute it from the
 		 * default timeout. It is now possible to use a different value
 		 * by changing readTimeout in ccid_open_hack_pre() */
-		ccid_descriptor->readTimeout = ccid_descriptor->readTimeout * 100.0 / DEFAULT_COM_READ_TIMEOUT;
+		ccid_descriptor->readTimeout = ccid_descriptor->readTimeout * 1000.0 / DEFAULT_COM_READ_TIMEOUT;
 
 		if ((IFD_COMMUNICATION_ERROR == CmdGetSlotStatus(reader_index, pcbuffer))
 			&& (IFD_COMMUNICATION_ERROR == CmdGetSlotStatus(reader_index, pcbuffer)))
